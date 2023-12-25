3b4e80907be9ba12ce73c1fd9015b3cbed7f552d
Aleksej Komnenović <dev@akom.me>
2023-12-25 23:09:53 +0100

    only show add record button on records page

--- components/general-components/top-nav-bar.tsx
+++ components/general-components/top-nav-bar.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { AddRecord } from "@/components/records-components/add-record-button";
+import { usePathname } from "next/navigation";
 
 export function TopNavBar() {
+  const selectedPage = usePathname();
   return (
     <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-      <AddRecord />
+      {selectedPage == "/records" ? <AddRecord /> : <div></div>}
     </header>
   );
 }
