4917cbb2d7d776ed55699a8d8150206096eb61aa
Aleksej Komnenović <dev@akom.me>
2023-12-27 17:43:22 +0100

    add playwright tests for records page

---
+++ tests/records-page.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/records");
+
+  await expect(page).toHaveTitle(/Qoluo/);
+});
+
+test("should navigate to the Records page", async ({ page }) => {
+  await page.goto("/records");
+  await page.click("text=Records");
+  await expect(page).toHaveURL("/records");
+  await expect(page.locator("h2")).toContainText("Records Overview");
+});
+
+test("should navigate to the Home page", async ({ page }) => {
+  await page.goto("/records");
+  await page.click("text=Home");
+  await expect(page).toHaveURL("/");
+  await expect(page.locator("h2")).toContainText("Accounts Overview");
+});
+
+test("should have Add Record button", async ({ page }) => {
+  await page.goto("/records");
+  await expect(page.getByText("Add Record")).toBeVisible();
+});
+
+test("should have a records table component", async ({ page }) => {
+  await page.goto("/records");
+  await expect(page.locator("#records-table")).toBeVisible();
+});
