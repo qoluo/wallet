20d344098eecb85d5719f90ad3b6137c5bb681d2
Aleksej Komnenović <dev@akom.me>
2024-01-01 01:16:22 +0100

    add internal api handler, utilize route handlers

---
+++ app/api/internal-api-handler-add-record/route.ts
@@ -0,0 +1,27 @@
+export async function POST(Request: Request) {
+  const requestBody = await Request.text();
+  const WALLET_API_URL = process.env.QOLUO_WALLET_API;
+  const WALLET_API_ADD_RECORDS_ENDPOINT =
+    process.env.QOLUO_WALLET_API_ADD_RECORDS_ENDPOINT;
+  console.log(requestBody);
+  try {
+    const response = await fetch(
+      `${WALLET_API_URL}/${WALLET_API_ADD_RECORDS_ENDPOINT}`,
+      {
+        method: "POST",
+        headers: Request.headers,
+        body: requestBody,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return new Response(data);
+  } catch (error) {
+    console.error("There was an error!", error);
+  }
+}
