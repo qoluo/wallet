6fdc213da906b0a29a4abf206ebf0bc4a8d96b3c
Aleksej Komnenović <dev@akom.me>
2024-01-07 23:04:45 +0100

    add toggle to the top bar

--- components/general-components/top-nav-bar.tsx
+++ components/general-components/top-nav-bar.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { AddRecord } from "@/components/records-components/add-record-button";
 import { usePathname } from "next/navigation";
+import { ModeToggle } from "@/components/general-components/dark-mode-toggle";
 
 export function TopNavBar() {
   const selectedPage = usePathname();
   return (
-    <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-      {selectedPage == "/records" ? <AddRecord /> : <div></div>}
-    </header>
+    <div>
+      <header className="flex justify-between h-14 lg:h-[60px] items-center gap-4 border-b  px-6 ">
+        {selectedPage == "/records" ? <AddRecord /> : <div />}
+        <ModeToggle />
+      </header>
+    </div>
   );
 }
