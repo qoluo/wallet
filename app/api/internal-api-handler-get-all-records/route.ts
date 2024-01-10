62b62f1911ac8baa7a7996030cfffdbe80a76450
Aleksej Komnenović <dev@akom.me>
2024-01-10 23:20:16 +0100

    add internal api handler for all records

---
+++ app/api/internal-api-handler-get-all-records/route.ts
@@ -0,0 +1,25 @@
+export async function POST(Request: Request) {
+  const WALLET_API_URL = process.env.QOLUO_WALLET_API;
+  const QOLUO_WALLET_API_GET_ALL_RECORDS_ENDPOINT =
+    process.env.QOLUO_WALLET_API_GET_ALL_RECORDS_ENDPOINT;
+
+  try {
+    const response = await fetch(
+      `${WALLET_API_URL}/${QOLUO_WALLET_API_GET_ALL_RECORDS_ENDPOINT}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response;
+  } catch (error) {
+    console.error("There was an error!", error);
+  }
+}
