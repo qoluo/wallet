c8f670516a22f8c22c8b3a28de6234d87b3a06fd
Aleksej Komnenović <dev@akom.me>
2024-01-11 01:02:18 +0100

    update fetch util for data retrieval

--- utils/records/get-all-records.ts
+++ utils/records/get-all-records.ts
@@ -1,10 +1,7 @@
-export async function getAllRecordsfromDB() {
-  const res = await fetch(
-    "http://localhost:3000/api/internal-api-handler-get-all-records",
-    {
-      method: "POST",
-    }
-  );
+export async function getAllRecordsfromDB(endpoint: string) {
+  const res = await fetch(endpoint, {
+    method: "POST",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data from internal API handler.");
