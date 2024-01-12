e3994397d7ee51dce1d66c7b2739c379dffd34f8
Aleksej Komnenović <dev@akom.me>
2024-01-12 01:04:40 +0100

    add id to records table for e2e testing

--- components/records-components/records-table.tsx
+++ components/records-components/records-table.tsx
@@ -21,9 +21,8 @@ export function RecordsTable() {
         </div>
       </div>
     );
-
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10" id="records-table">
       <RecordsDataTable columns={columns} data={data} />
     </div>
   );
