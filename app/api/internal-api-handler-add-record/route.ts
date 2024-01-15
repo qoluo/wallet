0bf1e7621fb6ca88c216375533900b5a8a86c596
Aleksej Komnenović <dev@akom.me>
2024-01-15 20:04:33 +0100

    Update route.ts

--- app/api/internal-api-handler-add-record/route.ts
+++ app/api/internal-api-handler-add-record/route.ts
@@ -7,7 +7,7 @@ export async function POST(Request: Request) {
     process.env.QOLUO_WALLET_API_ADD_RECORDS_ENDPOINT;
 
   try {
-    const request = await fetch(
+    const response = await fetch(
       `${WALLET_API_URL}/${WALLET_API_ADD_RECORDS_ENDPOINT}`,
       {
         method: "POST",
@@ -16,8 +16,8 @@ export async function POST(Request: Request) {
       }
     );
 
-    if (!request.ok) {
-      throw new Error(`HTTP error! status: ${request.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     return NextResponse.json({}, { status: 201 });
