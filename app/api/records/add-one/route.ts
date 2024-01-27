1558552315a0ff1d29253d66e496386b870a37e5
Aleksej Komnenović <dev@akom.me>
2024-01-27 23:56:21 +0100

    add new route handler for new record

---
+++ app/api/records/add-one/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { db, connection } from "@/utils/db/connect";
+import { records } from "@/utils/db/schema/records";
+
+export async function POST(Request: Request) {
+  const requestBody = await Request.json();
+
+  try {
+    const response = await db.insert(records).values({
+      type: requestBody.type,
+      account: requestBody.account,
+      amount: requestBody.amount,
+      currency: requestBody.currency,
+      record_happened_at: requestBody.date,
+      record_created_at: new Date().toISOString(),
+    });
+
+    //TODO: imrpove this error handling
+    if (response.count === undefined) {
+      throw new Error(`DB error! ${response.toString()}`);
+    }
+
+    return NextResponse.json({}, { status: 201 });
+  } catch (error) {
+    console.error("There was an error!", error);
+  }
+}
