ceeb9bc44673506044520e1c902c9ac227a92f8d
Aleksej Komnenović <dev@akom.me>
2024-01-28 00:38:25 +0100

    fix errors

--- utils/db/migrator.ts
+++ utils/db/migrator.ts
@@ -1,5 +1,9 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { db, connection } from "@/utils/db/connect";
 
-await migrate(db, { migrationsFolder: "sql-migration" });
-await connection.end();
+async function runMigration() {
+  await migrate(db, { migrationsFolder: "sql-migration" });
+  await connection.end();
+}
+
+runMigration();
