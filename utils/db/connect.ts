b248196c7c564468c4aa4d296a70f8a6b9d2bb5c
Aleksej Komnenović <dev@akom.me>
2024-01-28 00:55:21 +0100

    upgrade db connection

--- utils/db/connect.ts
+++ utils/db/connect.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-export const connection = postgres(constructConnectionString());
+export const connection = postgres(constructConnectionString(), {
+  prepare: false,
+});
 
 export const db = drizzle(connection);
 
